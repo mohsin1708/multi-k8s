ab66945da16b6068774b691ba0e429cdc541fde0
mohsin khan <you@example.com>
2020-07-13 00:15:56 +0100

    added files

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t mohsin17/multi-client:latest -t mohsin17/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t mohsin17/multi-server:latest -t mohsin17/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t mohsin17/multi-worker:latest -t mohsin17/multi-worker:$SHA ./worker/Dockerfile ./worker
+
+docker push mohsin17/multi-client:latest
+docker push mohsin17/multi-server:latest
+docker push mohsin17/multi-worker:latest
+
+docker push mohsin17/multi-client:$SHA
+docker push mohsin17/multi-server:$SHA
+docker push mohsin17/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubect set image deployments/server-deployment server=mohsin17/multi-server:$SHA
+kubect set image deployments/client-deployment server=mohsin17/multi-client:$SHA
+kubect set image deployments/worker-deployment server=mohsin17/multi-worker:$SHA
