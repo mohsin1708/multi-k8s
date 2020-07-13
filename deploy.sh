6a1fa66e81dc36f86f451a0d55ee3c36a9e85d7c
mohsin khan <you@example.com>
2020-07-13 01:57:10 +0100

    added right files

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-docker build -t mohsin17/multi-client:$SHA ./client/Dockerfile ./client
-docker build -t mohsin17/multi-server:$SHA ./server/Dockerfile ./server
-docker build -t mohsin17/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker build -t mohsin17/multi-client:latest -t mohsin17/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mohsin17/multi-server:latest -t mohsin17/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mohsin17/multi-worker:latest -t mohsin17/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
+docker push mohsin17/multi-client:latest
+docker push mohsin17/multi-server:latest
+docker push mohsin17/multi-worker:latest
 
 docker push mohsin17/multi-client:$SHA
 docker push mohsin17/multi-server:$SHA
@@ -12,4 +15,4 @@ docker push mohsin17/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=mohsin17/multi-server:$SHA
 kubectl set image deployments/client-deployment client=mohsin17/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=mohsin17/multi-worker:$SHA
+kubectl set image deployments/worker-deployment work=mohsin17/multi-worker:$SHA
