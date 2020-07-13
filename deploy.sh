870ef86efb7f94265bf6077ad5fac3db6a76a95f
mohsin khan <you@example.com>
2020-07-13 01:49:27 +0100

    added new files 13

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-docker build -t mohsin17/multi-client:latest -t mohsin17/multi-client:$SHA ./client/Dockerfile ./client
-docker build -t mohsin17/multi-server:latest -t mohsin17/multi-server:$SHA ./server/Dockerfile ./server
-docker build -t mohsin17/multi-worker:latest -t mohsin17/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker build -t mohsin17/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t mohsin17/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t mohsin17/multi-worker:$SHA ./worker/Dockerfile ./worker
 
-docker push mohsin17/multi-client:latest
-docker push mohsin17/multi-server:latest
-docker push mohsin17/multi-worker:latest
 
 docker push mohsin17/multi-client:$SHA
 docker push mohsin17/multi-server:$SHA
